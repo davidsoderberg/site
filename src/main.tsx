e311c41af6fd14e68dfcab433096c2927b493957
David Söderberg <granskog_@hotmail.com>
2024-07-05 08:47:25 +0200

    Add Vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 import './index.css';
 import { Wrapper } from './components/Wrapper.tsx';
@@ -55,5 +56,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <RouterProvider router={router} />
     </Wrapper>
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
